bf7d68e198b2fbd5a3547d5e9dcb290c5f03ba1f
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-12-13 10:24:16 +0530

    Add axios and configur it for API communication.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from './backend/vue-axios'
 
 Vue.config.productionTip = false
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  axios,
   render: h => h(App)
 })
